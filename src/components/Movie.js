5b0a620890722b72b2c298831ce0dfd92f1ab866
david <david@MacBook-Pro-de-david.local>
2020-02-04 00:40:20 +0100

    mymoviz part 2

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../App.css';
 import { 
   Button,
@@ -17,13 +17,73 @@ import { faHeart, faStar, faVideo} from '@fortawesome/free-solid-svg-icons'
 
 function Movie(props) {
 
+  const [likeMovie, setLikeMovie] = useState(false)
+  const [watchMovie, setWatchMovie] = useState(false)
+  const [countWatchMovie, setCountWatchMovie] = useState(0)
+  const [myRatingMovie, setMyRatingMovie] = useState(0)
+  const [isRatingMovie, setIsRatingMovie] = useState(false)
+
+  const [rating, setRating] = useState(props.globalRating)
+  const [countRating, setCountRating] = useState(props.globalCountRating)
+
+  var addWatch = () => {
+    setWatchMovie(true)
+    setCountWatchMovie(countWatchMovie+1)
+  }
+
+  var setMyRating = (rating) => {
+    if(rating < 0){
+      rating = 0
+    }
+
+    if(rating > 10){
+      rating = 10
+    }
+
+    setMyRatingMovie(rating)
+    setIsRatingMovie(true)
+  }
+
+  var tabRating = []
+  for(var i=0;i<10;i++){
+      var color = {}
+      if(i<myRatingMovie){
+          color = {color: '#f1c40f'}
+      }
+      let count = i+1
+      tabRating.push(<FontAwesomeIcon onClick={() => setMyRating(count)} style={color} icon={faStar} /> )
+  }
+
+  var nbTotalNote = rating * countRating
+  var nbTotalVote = countRating
+
+  if(isRatingMovie){
+    nbTotalVote +=1
+    nbTotalNote += myRatingMovie
+  }
+
+  var avgTotal = Math.round(nbTotalNote / nbTotalVote)
+  console.log(avgTotal)
+
   var tabGlobalRating = []
   for(var i=0;i<10;i++){
       var color = {}
-      if(i<props.globalRating){
+      if(i<avgTotal){
           color = {color: '#f1c40f'}
       }
       tabGlobalRating.push(<FontAwesomeIcon style={color} icon={faStar} /> )
+  }
+
+  if(likeMovie){
+    var colorLike = {color: '#e74c3c',cursor:'pointer'}
+  } else {
+    var colorLike = {cursor:'pointer'}
+  }
+
+  if(watchMovie){
+    var colorWatch = {color: '#e74c3c'}
+  } else {
+    var colorWatch = {}
   }
 
   return (
@@ -31,28 +91,19 @@ function Movie(props) {
     <Card style={{marginBottom:30}}>
     <CardImg top src={props.movieImg} alt={props.movieName} />
     <CardBody>
-        <p>Like <FontAwesomeIcon style={{cursor:'pointer'}} icon={faHeart} /></p>
-        <p>Nombre de vues  <FontAwesomeIcon icon={faVideo} /> <Badge color="secondary">2</Badge></p>
+        <p>Like <FontAwesomeIcon style={colorLike} icon={faHeart} onClick={() => setLikeMovie(!likeMovie)} /></p>
+  <p>Nombre de vues  <FontAwesomeIcon style={colorWatch} icon={faVideo} onClick={() => addWatch()} /> <Badge color="secondary">{countWatchMovie}</Badge></p>
         <p>Mon avis 
-        <FontAwesomeIcon icon={faStar} /> 
-        <FontAwesomeIcon icon={faStar} /> 
-        <FontAwesomeIcon icon={faStar} /> 
-        <FontAwesomeIcon icon={faStar} /> 
-        <FontAwesomeIcon icon={faStar} /> 
-        <FontAwesomeIcon icon={faStar} /> 
-        <FontAwesomeIcon icon={faStar} /> 
-        <FontAwesomeIcon icon={faStar} /> 
-        <FontAwesomeIcon icon={faStar} /> 
-        <FontAwesomeIcon icon={faStar} /> 
+        {tabRating}
 
         <ButtonGroup size="sm">
-            <Button color="secondary">-</Button>
-            <Button color="secondary">+</Button>
+            <Button onClick={() => setMyRating(myRatingMovie-1)} color="secondary">-</Button>
+            <Button onClick={() => setMyRating(myRatingMovie+1)} color="secondary">+</Button>
         </ButtonGroup>
         </p>
         <p>Moyenne
         {tabGlobalRating}
-        ({props.globalCountRating})
+        ({nbTotalVote})
         </p>
   <CardTitle>{props.movieName}</CardTitle>
         <CardText>{props.movieDesc}</CardText>
